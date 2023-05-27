77fe4268de59b67bab07f6671fc8555f342d83fb
RohanRao1 <74089730+RohanRao1@users.noreply.github.com>
2023-05-28 01:00:04 +0530

    modifying the store data using dispatch and useSelector

--- App.js
+++ App.js
@@ -1,42 +1,9 @@
-import "./App.css";
-import React, { useContext } from "react";
-import Login from "./components/authentication/Login";
-import WelcomePage from "./components/pages/WelcomePage";
-import { Route, Switch, Redirect } from "react-router-dom";
-import AuthContext from "./components/auth/AuthContext";
-import ProfilePage from "./components/pages/ProfilePage";
-import ResetPassword from "./components/pages/ResetPassword";
+import Counter from './components/Counter';
+
 
 function App() {
-  const authctx = useContext(AuthContext)
-
-  const isLoggedIn = authctx.isLoggedIn
-  
   return (
-    <React.Fragment>
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/login" />
-        </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
-        {isLoggedIn && (
-          <Route path="/Welcomepage" exact>
-            <WelcomePage />
-          </Route>
-        )}
-        {isLoggedIn && (
-          <Route path="/Welcomepage/profile">
-            <ProfilePage />
-          </Route>
-        )}
-        <Route path='/resetpassword'>
-          <ResetPassword />
-        </Route>
-        <Route path="*">Page Not Found</Route>
-      </Switch>
-    </React.Fragment>
+    <Counter />
   );
 }
 
--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {Provider } from 'react-redux'
+
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import { AuthContextProvider } from './components/auth/AuthContext';
+import store from './store/index';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
-  </BrowserRouter>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
