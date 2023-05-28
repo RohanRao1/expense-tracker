1bba05cff0fb53112de6bc5b75386dbce7b550a9
RohanRao1 <74089730+RohanRao1@users.noreply.github.com>
2023-05-28 15:33:18 +0530

    adding dummy auth using redux

--- Counter.js
+++ Counter.js
@@ -1,27 +1,36 @@
 import classes from './Counter.module.css';
 import {useSelector, useDispatch } from 'react-redux'
+import { counterActions} from '../store/index'
 
 const Counter = () => {
- const counter =  useSelector(state => state.counter)
+ const counter =  useSelector(state => state.counter.counter)
  const dispatch = useDispatch()
+  const show = useSelector(state => state.counter.showCounter)
 
  const incrementHandler = () => {
-  dispatch({type : 'increment' })
+  dispatch(counterActions.increment())
+ }
+
+ const increaseHandler = () => {
+  dispatch(counterActions.increase(2)) // {type : some_id, payload : 2}
  }
 
  const decrementHandler =() => {
-  dispatch({type : 'decrement'})
+  dispatch(counterActions.decrement())
  }
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggle())
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div> }
       <div>
-        <button onClick={incrementHandler}>Increment By 5</button>
-        <button onClick={decrementHandler}>Decrement By 5</button>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increment by 2</button>
+        <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
--- index.js
+++ index.js
@@ -1,14 +1,88 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {Provider } from 'react-redux'
+//  import { createStore } from 'redux'
+ import { createSlice, configureStore } from '@reduxjs/toolkit'
 
-import './index.css';
-import App from './App';
-import store from './store/index';
+ const initialCounterState = {
+    counter : 0,
+    showCounter : true
+ }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+const counterSlice = createSlice({
+       name : 'counter' , 
+       initialState :  initialCounterState ,
+       reducers : {
+        increment(state) {
+            state.counter++
+        },
+        decrement(state) {
+            state.counter--
+        },
+        increase(state,action) {
+            state.counter = state.counter + action.payload
+        },
+        toggle(state) {
+            state.showCounter = !state.showCounter
+        }
+       }
+})
+
+const initialAuthState = {
+    isAuthenticated : false
+}
+
+const AuthSlice = createSlice({
+    name : 'authentication',
+    initialState : initialAuthState ,
+    reducers : {
+        login (state) {
+            state.isAuthenticated = true
+        } ,
+        logout(state) {
+            state.isAuthenticated = false
+        }
+    }
+})
+
+//  const counterReducer = (state = initialState, action) => {
+//     if(action.type === 'increment'){
+//         return {
+//             counter : state.counter  + 1,
+//             showCounter : state.showCounter
+//         }
+//     }
+
+//     if (action.type=== 'increase'){
+//         return {
+//           counter: state.counter + action.amount,
+//           showCounter: state.showCounter,
+//         };
+//     }  
+
+//     if (action.type === 'decrement'){
+//         return {
+//           counter: state.counter - 1,
+//           showCounter: state.showCounter,
+//         };
+//     }
+
+//     if (action.type === 'toggle'){
+//         return {
+//             counter : state.counter,
+//             showCounter : !state.showCounter
+//         }
+//     }
+
+//     return state
+//  }
+
+
+
+//  const store = createStore(counterSlice.reducer)
+const store = configureStore({
+    reducer : {counter : counterSlice.reducer,
+            auth : AuthSlice.reducer }
+})
+
+export const counterActions = counterSlice.actions
+export const authActions = AuthSlice.actions
+
+export default store
