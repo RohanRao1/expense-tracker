24eb1d9eb23f8f2dba451af38739a343b741387b
RohanRao1 <74089730+RohanRao1@users.noreply.github.com>
2023-06-01 13:26:37 +0530

    adding activate premium feature

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,51 @@
-import { configureStore } from "@reduxjs/toolkit";
-import authSlice from './authentication'
-import expenseSlice from './expenses'
+//  import { createStore } from 'redux'
+ import {  configureStore } from '@reduxjs/toolkit'
+ import AuthReducer from './auth'
+ import counterReducer from './counter'
 
+ 
+
+//  const counterReducer = (state = initialState, action) => {
+//     if(action.type === 'increment'){
+//         return {
+//             counter : state.counter  + 1,
+//             showCounter : state.showCounter
+//         }
+//     }
+
+//     if (action.type=== 'increase'){
+//         return {
+//           counter: state.counter + action.amount,
+//           showCounter: state.showCounter,
+//         };
+//     }  
+
+//     if (action.type === 'decrement'){
+//         return {
+//           counter: state.counter - 1,
+//           showCounter: state.showCounter,
+//         };
+//     }
+
+//     if (action.type === 'toggle'){
+//         return {
+//             counter : state.counter,
+//             showCounter : !state.showCounter
+//         }
+//     }
+
+//     return state
+//  }
+
+
+
+//  const store = createStore(counterSlice.reducer)
 const store = configureStore({
-    reducer : {authentication : authSlice,
-            expense : expenseSlice
-    }
+    reducer : {counter : counterReducer,
+            auth : AuthReducer }
 })
+
+
+
 
 export default store
